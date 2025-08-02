b7aa54683e2568603c29b4ecb1013eebb59c05bd
waqasansariii <159879624+waqasansariii@users.noreply.github.com>
2025-08-02 12:53:35 +0500

    changing in server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
-const serverless = require('serverless-http');
 const express = require('express');
-const connecToDB = require("./database/db.js");
-const authRoutes = require("./routes/auth-routes.js");
-const homeRoutes = require("./routes/home-routes.js");
-const admin = require("./routes/admin-routes.js");
-const uploadImageRoutes = require("./routes/image-routes.js");
+const serverless = require('serverless-http');
+
+const connectToDB = require('./database/db.js');
+const authRoutes = require('./routes/auth_routes.js');
+const homeRoutes = require('./routes/home_routes.js');
+const adminRoutes = require('./routes/admin_routes.js');
+const uploadImageRoutes = require('./routes/image_routes.js');
 
 const app = express();
-// Middlewares
-app.use(express.json()); // for parsing application/json
-app.use('/api/auth',authRoutes);
-app.use('/api/home',homeRoutes);
-app.use('/api/admin',admin)
-app.use('/api/image',uploadImageRoutes);
-// Connect to the database
-connecToDB();
-const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT,()=>{
-//     console.log(`Server is now liestening to port ${PORT}`)
-// })  it is not support on the deployment on vercel
-module.exports = serverless(app);
+// Connect to DB
+connectToDB();
+
+// Middlewares
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/home', homeRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/image', uploadImageRoutes);
+
+module.exports = app;
+module.exports.handler = serverless(app); // ✅ only this for serverless
